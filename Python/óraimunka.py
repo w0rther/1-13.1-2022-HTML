93b0d1946df44077ba505f68a032125c7d926afc
w0rther <pfandlerricsi@gmail.com>
2022-11-08 15:50:18 +0100

    Python

---
+++ Python/óraimunka.py
@@ -0,0 +1,13 @@
+import turtle
+ablak = turtle.Screen()
+ablak.title("Hello, Richárd!")
+ablak.bgcolor("pink")
+Sanyi = turtle.Turtle()
+Sanyi.color("white")
+Sanyi.pensize(5)
+Sanyi.forward(50)
+Sanyi.left (90)
+Sanyi.forward(30)
+
+
+ablak.mainloop()
---
+++ Python/óraimunka2.py
@@ -0,0 +1,35 @@
+import turtle
+ablak = turtle.Screen()
+ablak.bgcolor("lightgreen")
+ablak.title("Ricsi & Armi")
+
+Ricsi = turtle.Turtle()
+Ricsi.color("hotpink")
+Ricsi.pensize(5)
+
+Armand = turtle.Turtle()
+
+Ricsi.forward(80)
+Ricsi.left(120)
+Ricsi.forward(80)
+Ricsi.left(120)
+Ricsi.forward(80)
+Ricsi.left(120)
+
+Ricsi.right(180)
+Ricsi.forward(80)
+
+Armand.forward(50)
+Armand.left(90)
+Armand.forward(50)
+Armand.left(90)
+Armand.forward(50)
+Armand.left(90)
+Armand.forward(50)
+Armand.left(90)
+
+
+ablak.mainloop()
+
+
+
