477422acf32916a7600216d37ba20ce064cdd451
w0rther <pfandlerricsi@gmail.com>
2022-11-13 22:42:08 +0100

    asdasdada

---
+++ Python/2.14.py
@@ -0,0 +1,25 @@
+
+#1
+a = "Lustaság " 
+b = "fél "
+c = "egészség"  
+print(a+b+c)
+#2
+print(6*(1-2))
+#4
+Pista = 6
+print (Pista + 4)
+#5
+d = 10000
+e = 8
+f = 12
+t = int(input("évek száma?"))
+print(d*(1+e/f)**t)
+
+#7
+print(14+(51%int(51/12)))
+#8
+ora1= int(input("Hány óra van?"))
+ora2= int(input("Hány órával később szólaljón meg?"))
+print(ora1+(ora2%int(ora2/12)))
+
