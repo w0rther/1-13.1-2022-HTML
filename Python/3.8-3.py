2a54b453922d1f23bf21403f059a57e4163d67fa
w0rther <pfandlerricsi@gmail.com>
2022-11-14 12:30:40 +0100

    asd

---
+++ Python/3.8-3.py
@@ -0,0 +1,6 @@
+hónapok = ["Január","Február","Március","Április","Május","Június","Július","Augusztus","Szeptember"
+        ,"Október","November","December"]
+
+for hónap in hónapok:
+    print("Az év egyik hónapja", hónap)
+
