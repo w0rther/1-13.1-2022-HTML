bf12f2bc36d7ed10d45111127ee9385681337437
w0rther <pfandlerricsi@gmail.com>
2022-11-21 16:32:23 +0100

    Kész

---
+++ Python/3.8-2.py
@@ -0,0 +1,11 @@
+class Mobil:
+    def Csörög():
+        print("Csörög a telefon")
+    def Bekapcsol():
+        print("Bekapcsol a telefon")
+    def Kikapcsol():
+        print("Kikapcsol a telefon")
+
+Mobil.Csörög()
+Mobil.Bekapcsol()
+Mobil.Kikapcsol()
