371566ab020197b4c3193073b8d2ad31c700ce5d
w0rther <pfandlerricsi@gmail.com>
2022-12-13 10:31:20 +0100

    HTML

---
+++ Vizsga/Python/1.py
@@ -0,0 +1,10 @@
+szam1 = int(input('Adja meg az első számot: '))
+szam2 = int(input('Adja meg a második számot: '))
+
+if szam1 or szam2 >0:
+          print('A két szám közül egyik sem negatív')
+else:
+         print('A két szám közül mindkettő negatív')
+
+    
+ 
