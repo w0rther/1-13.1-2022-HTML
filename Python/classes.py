e98bb01098dfc5670e240b2249ac2aa22b583d75
w0rther <pfandlerricsi@gmail.com>
2022-12-15 11:50:16 +0100

    ki

---
+++ Python/classes.py
@@ -0,0 +1,22 @@
+import math
+
+class Negyzet:
+    def __init__(self, a = 0):
+        self.a = a
+    
+    def SetA(self, a):
+        self.a = a
+
+    def getKerulet(self):
+        return 4 * self.a
+
+    def getTerulet (self):
+        return math.pow(self.a, 2)
+
+
+class Kor:
+    def __init__(self, a )
+    
+
+
+
