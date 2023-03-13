e0972b3a4915ce31ec9ec052e9b5a9f7ca5f2736
pfandler.ricsi <pfandlerricsi@gmail.com>
2023-03-13 17:22:14 +0100

    c

--- Mappa/TTomegOOP/TestTomeg.cs
+++ Mappa/TTomegOOP/TestTomeg.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace TTomegOOP
 {
+    struct Testtomeg
+    {
+        public string tomeg; // A testtömeg nav
+    }
+    struct TestMagasság
+    {
+        public string magass;
+    }
     internal class TestTomeg
     {
-    }
+        private int[,] testtomeg = null;
+
+        public TestTomeg(int[,] p)
+     {
+         this.testtomeg = p;
+     }
+
+        // Tömeg bekérés
+
+        // Súly bekérés
+
+        //Testtömeg index kiszámítása
+
+        private void testtomeg()
+
+        {
+            int testtomeg = 0;
+            if (testtomeg < 10)
+            {
+                Console.WriteLine("Súlyosan soványság");
+
+            }
+            else if (testtomeg < 17)
+            {
+                Console.WriteLine("Mérsékelt soványság");
+
+            }
+            else if (testtomeg < 18.5)
+            {
+                Console.WriteLine("Enyhe soványság");
+
+            }
+            else if (testtomeg < 25)
+            {
+                Console.WriteLine("Normális soványság");
+
+            }
+            else if (testtomeg < 30)
+            {
+                Console.WriteLine("Túlsúlyos");
+            }
+        }
+        }
 }
